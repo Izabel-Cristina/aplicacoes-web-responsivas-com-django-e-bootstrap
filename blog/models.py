ec45eba5a265f59ba932831b7787b18723d2c8d5
Izabel Cristina <izabelcrigo@gmail.com>
2023-01-09 21:18:31 -0300

    Commit - Configuração de arquivos estáticos e configuração do CKEditor.

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 # Create your models here.
 class Post(models.Model):
+    objects = None
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
-    conteudo = models.TextField()
+    conteudo = RichTextUploadingField(blank=False, null=False)
     data_publicacao = models.DateTimeField(default=timezone.now)
 
-#Função para exibir na tela o titulo.
+    # Função para exibir na tela o titulo.
     def __str__(self):
         return self.titulo
