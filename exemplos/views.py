eb024b11b09882ca4eef2fc7f9c710fc7c6c04c2
Izabel Cristina <izabelcrigo@gmail.com>
2023-01-12 20:50:10 -0300

    Commit - 1. Header; 2-Desenvolvendo o footer do blog;

--- exemplos/views.py
+++ exemplos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def get_bootstrap(request):
-    return render(request, 'exemplos/12_utilitarios.html')
+    return render(request, 'blog/home.html')
