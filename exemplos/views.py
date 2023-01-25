31327f97551d3dcf0ba1d4de309870fcfd010c80
Izabel Cristina <izabelcrigo@gmail.com>
2023-01-25 20:37:02 -0300

    Commit- 1.Iniciando a validação de dados

--- exemplos/views.py
+++ exemplos/views.py
@@ -1,10 +1,48 @@
-from django.shortcuts import render,HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, HttpResponse
+import re
 
 # Create your views here.
 def get_bootstrap(request):
-    return render(request, 'exemplos/16_forms_parte_I.html')
+    return render(request, 'exemplos/16_forms_parte_i.html')
+
+#Validar senha:
+def validou_senha(senha):
+    regex ='^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{4,15}$'
+    if (re.search(regex,senha)):
+        return True
+    else:
+        return False
+
+def validou_email(email):
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if (re.search(regex,email)):
+        return True
+    else:
+        return False
+def validou_form(email,senha):
+    if validou_email(email) and validou_senha(senha):
+        return True
+    else:
+        return False
+
 
 def processa_formulario_v1(request):
     email = request.POST.get("email")
-    password = request.POST.get("senha")
-    return HttpResponse("{}----- e-----{}".format(email,password))
+    senha = request.POST.get("senha")
+
+    email_st = 'is_valid'
+    senha_st = 'is_valid'
+
+    context = {
+        "email": email,
+        "senha": senha
+
+    }
+
+    if validou_form(email,senha):
+        return HttpResponseRedirect("/")
+    else:
+       return render(request, 'exemplos/16_forms_parte_i.html', context)
+
+
